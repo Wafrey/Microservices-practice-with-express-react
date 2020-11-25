a1fbe8c465c71cfdc170ca7c2e38120a5e8a6de8
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-11-25 22:03:22 +0200

    create express event-bus, and handle emitting events from comments, and posts

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,7 +15,7 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   // generate random id to handle creation of post
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
@@ -23,6 +24,14 @@ app.post("/posts", (req, res) => {
     id,
     title,
   };
+
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
 
   res.status(201).send(posts[id]);
 });
