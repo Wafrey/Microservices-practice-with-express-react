81c27e26a76fdb0e80cc885ae46368692e68cbf5
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-11-28 18:55:32 +0200

    change commentService to handle moderationService

--- comments/index.js
+++ comments/index.js
@@ -40,6 +40,27 @@ app.post("/posts/:id/comments", async (req, res) => {
 app.post("/events", async (req, res) => {
   console.log("Received event", req.body.type);
 
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    const { id, postId, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        postId,
+        status,
+        content,
+      },
+    });
+  }
   res.send({});
 });
 
